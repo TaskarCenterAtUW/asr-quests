9946f4e646ccdd2d9abfa6d952ddfa13283c5e3b
Amy Kate Bordenave <amykateb@uw.edu>
2026-05-05 18:37:28 -0700

    add main.js

---
+++ creator/src/main.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from "./App.vue";
+
+const app = createApp(App);
+app.use(createPinia());
+app.mount("#app");
