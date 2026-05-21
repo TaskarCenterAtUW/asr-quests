e45ee9d0797dcec3669d808548f61f915a3dc05f
Amy Kate Bordenave <amykateb@uw.edu>
2026-05-21 00:58:28 -0700

    fix base path

--- creator/vite.config.js
+++ creator/vite.config.js
@@ -6,5 +6,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/asr-quests/creator/", // ← matches https://taskarcenteratuw.github.io/asr-quests/creator/
+  base: "/asr-quests/",
 });
